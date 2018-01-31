5d58f94672a491eb900843ae9373dfd9b9df6508
abelzhang <abelzhang@ABELZHANG-PC3>
2018-01-31 11:50:51 +0800

    Initial commit

---
+++ letsgo.py
@@ -0,0 +1,20 @@
+import xlwings as xw
+
+print("hello world")
+
+app = xw.App(visible=False, add_book=False)
+app.display_alerts = False
+app.screen_updating = False
+
+wb = app.books.add()
+wb.sheets.add(after=wb.sheets[wb.sheets.count-1])
+wb.sheets.add(after=wb.sheets[wb.sheets.count-1])
+print(wb.sheets.count)
+
+wb.sheets[0].range('A1').value = "hello world"
+wb.sheets[1].range('A1').value = "hello world"
+
+wb.save(r"test.xlsx")
+wb.close()
+app.quit()
+print("goodbye world")
